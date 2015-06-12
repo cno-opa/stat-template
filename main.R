16e6eedc1f4deba3cf7868ca1c08360151b45025
Conor Gaffney <conorsg@gmail.com>
2015-06-12 16:27:58 -0500

    source gfx and map fns from Github instead of local versions

--- main.R
+++ main.R
@@ -13,7 +13,23 @@ init <- function(subdir) {
   cat("Running!\n")
 }
 
+#fn to source files stored on Github
+source_https <- function(u, unlink.tmp.certs = FALSE) {
+    # load package
+    require(RCurl)
+
+    # read script lines from website using a security certificate
+    if(!file.exists("cacert.pem")) download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile = "cacert.pem")
+    script <- getURL(u, followlocation = TRUE, cainfo = "cacert.pem")
+    if(unlink.tmp.certs) unlink("cacert.pem")
+
+    # parase lines and evealuate in the global environement
+    eval(parse(text = script), envir= .GlobalEnv)
+}
+
 # sequence of script executions
+source_https("https://raw.githubusercontent.com/cno-opa/graphics/master/plotters.R")
+source_https("https://raw.githubusercontent.com/cno-opa/graphics/master/mappers.R")
 init("R/lib")
 init("R")
 
